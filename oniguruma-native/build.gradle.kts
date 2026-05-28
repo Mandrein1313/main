a870a6aa3776a51c3e2e6566395fc1f51ad5c057
Mandrein <145546234+Mandrein1313@users.noreply.github.com>
2026-05-28 13:41:38 +0700

    Update build.gradle.kts

--- oniguruma-native/build.gradle.kts
+++ oniguruma-native/build.gradle.kts
@@ -1,8 +1,46 @@
-ndkVersion = "29.0.14206865"
+/*******************************************************************************
+ *    sora-editor - the awesome code editor for Android
+ *    https://github.com/Rosemoe/sora-editor
+ ******************************************************************************/
 
-externalNativeBuild {
-    cmake {
-        path = project.file("src/main/cpp/CMakeLists.txt")
-        version = "3.31.5"        // ← เปลี่ยนเป็นเวอร์ชันใหม่
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.publish)
+}
+
+android {
+    namespace = "io.github.rosemoe.sora.oniguruma"
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    // ==================== ส่วน NDK + CMake ====================
+    ndkVersion = "29.0.14206865"
+
+    externalNativeBuild {
+        cmake {
+            path = "src/main/cpp/CMakeLists.txt"   // ใช้ String
+            version = "3.31.5"
+        }
+    }
+    // =======================================================
+
+}
+
+dependencies {
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.junit)
+    androidTestImplementation(libs.androidx.test.espresso)
 }
